6344b8031072e7c20d9652ea2fcbb37919c87354
Arash Laali <arashlaali@Arashs-MacBook-Pro.local>
2025-03-02 10:46:21 +0330

    added contact form and text to contact page ,installed contact form pluging

---
+++ assets/fonts/js/contact.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const button = document.getElementById('scroll-top');
+    button.style.color = 'var(--wp--preset--color--primary)';
+});
+
---
+++ mindset-blocks/build/company-address/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '038ea6f0d33ea0e093fd');
